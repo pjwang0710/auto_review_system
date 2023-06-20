b743aaaf26e4cff52582c7ad65a2c6b47ba77996
explainthis.io <hello@explainthis.io>
2023-06-20 22:50:26 +0800

    Remove ping test

--- app/utils/mongodb.py
+++ app/utils/mongodb.py
@@ -20,7 +20,6 @@ class MongoDB():
         mongo_path = mongo_path or os.getenv('MONGODB__PATH')
         db_name = db_name or os.getenv('MONGODB__DBNAME')
         self.client = MongoClient(mongo_path)
-        assert self.client.config.command('ping')['ok'] == 1.0
         self.db = self.client[db_name]
 
 
