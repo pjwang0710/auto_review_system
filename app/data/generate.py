032f96292f236c17bc3005607bfd1b6bc9464d23
explainthis.io <hello@explainthis.io>
2023-06-20 22:48:43 +0800

    add run

--- app/data/generate.py
+++ app/data/generate.py
@@ -1,7 +1,16 @@
 from app.utils.mongodb import mongodb
 from app.data.batch_35 import students, assignments
+import os
+from dotenv import load_dotenv
+
+load_dotenv(f'.{os.getenv("MODE")}.env')
 
 
 def run():
     mongodb.db['students'].insert_many(students)
     mongodb.db['assignments'].insert_many(assignments)
+
+
+if __name__ == '__main__':
+    mongodb.connect_to_database()
+    run()
