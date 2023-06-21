5495991b261344c8dfef9e9ba136993defc3f522
explainthis.io <hello@explainthis.io>
2023-06-21 15:39:52 +0800

    add synchronize type

--- app/api/v1/endpoints/monitor.py
+++ app/api/v1/endpoints/monitor.py
@@ -19,15 +19,12 @@ async def add_progresses(request: Request, db=Depends(get_database)) -> Any:
     uri = None
     validate_type = None
     pr_number = None
-    print('=====================')
-    print(payload.get('action'))
-    print('=====================')
     if payload.get('action') == 'closed' and not payload.get('pull_request', {}).get('merged_at'):
         validate_type = VALIDATE_TYPES.CLOSED
     elif payload.get('pull_request'):
         if payload.get('pull_request', {}).get('merged_at'):
             validate_type = VALIDATE_TYPES.MERGE
-        elif payload.get('action') == 'opened':
+        elif payload.get('action') == 'opened' or payload.get('action') == 'synchronize':
             pr_number = payload.get('pull_request', {}).get('number')
             validate_type = VALIDATE_TYPES.PULL_REQUEST
         uri = payload.get('pull_request', {}).get('issue_url') + '/comments'
